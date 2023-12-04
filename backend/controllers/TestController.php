e1e2b62c3c9fbb45d4dd75c69620b1020bf7baa5
xxxtentacionuz <samariddinxacker@gmail.com>
2023-12-04 12:44:43 +0500

    baza

---
+++ backend/controllers/TestController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace backend\controllers;
+
+use common\models\Aboutme;
+use backend\models\AboutmeSearch;
+use mysqli;
+use Yii;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\web\UploadedFile;
+
+/**
+ * AboutmeController implements the CRUD actions for Aboutme model.
+ */
+class TestController extends Controller
+{
+    public function actionIndex()
+    {
+            $servername = "localhost";
+            $username = "root";
+            $password = "yangiparol";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password);
+
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            echo "Connected successfully";
+    }
+}
